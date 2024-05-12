3b648e0c2c68bf818acb75503b9556e8688c65e6
Demali-876 <90882773+Demali-876@users.noreply.github.com>
2024-05-12 17:33:43 -0500

    II integration

--- src/wallet_integrations_frontend/src/routes/+page.svelte
+++ src/wallet_integrations_frontend/src/routes/+page.svelte
@@ -1,15 +1,44 @@
 <script>
-  import "../index.scss";
+  import "../index.scss"; 
+  import { createActor, canisterId } from 'declarations/wallet_integrations_backend';
+  import { writable } from 'svelte/store';
   import { backend } from "$lib/canisters";
+  import { AuthClient } from "@dfinity/auth-client";
+  import { HttpAgent } from "@dfinity/agent";
 
-  let greeting = "";
+  let actor = backend;
+  let principal = writable("");
+  let greeting = writable("");
+
+  async function handleWhoAmI() {
+      const principalResult = await actor.whoami();
+      principal.set(principalResult.toString());
+  };
+
+  async function handleLogin() {
+      let authClient = await AuthClient.create();
+      await new Promise((resolve) => {
+          authClient.login({
+              identityProvider:
+                  process.env.DFX_NETWORK === "ic"
+                      ? "https://identity.ic0.app"
+                      : `http://rdmx6-jaaaa-aaaaa-aaadq-cai.localhost:4943`,
+              onSuccess: resolve,
+          });
+      });
+      const identity = authClient.getIdentity();
+      const agent = new HttpAgent({ identity });
+      actor = createActor(canisterId, {
+          agent,
+          
+      });
+  };
 
   function onSubmit(event) {
-    const name = event.target.name.value;
-    backend.greet(name).then((response) => {
-      greeting = response;
-    });
-    return false;
+      const name = event.target.name.value;
+      backend.greet(name).then(response => {
+        greeting.set(response);
+      });
   }
 </script>
 
@@ -17,10 +46,19 @@
   <img src="/logo2.svg" alt="DFINITY logo" />
   <br />
   <br />
-  <form action="#" on:submit|preventDefault={onSubmit}>
+  <form on:submit|preventDefault={onSubmit}>
     <label for="name">Enter your name: &nbsp;</label>
     <input id="name" alt="Name" type="text" />
     <button type="submit">Click Me!</button>
   </form>
-  <section id="greeting">{greeting}</section>
+  <section>{$greeting}</section>
+  <form>
+    <button type="button" on:click={handleLogin}>Login!</button>
+  </form>
+  <br />
+  <form>
+    <button type="button" on:click={handleWhoAmI}>Who Am I</button>
+  </form>
+  <section>{$principal}</section>
 </main>
+
